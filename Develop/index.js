b5ca221c27e4c69f5946e74d4cc71ca394b7d6be
Cadence K <cadencehiggins@gmail.com>
2024-05-16 18:28:48 -0500

    fixed an issue with port

--- Develop/index.js
+++ Develop/index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 const db = require('./db/db.json')
 const fs = require('fs')
 const path = require('path')
